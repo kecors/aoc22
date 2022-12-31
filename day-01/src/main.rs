4a3078ed7383fb5f71a722e2785b4d1218f454aa
Kenneth Collins <dev@kecors.net>
2022-12-31 16:42:37 -0500

    Solve day 1, part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -4,21 +4,32 @@ fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
 
-    let values: Vec<Option<u32>> = input
+    let mut values: Vec<Option<u32>> = input
         .lines()
         .map(|line| match line {
             "" => None,
             _ => Some(line.trim().parse::<u32>().unwrap()),
         })
         .collect();
+    // Mark end of last group
+    values.push(None);
 
-    let (_total, max_total) =
-        values
-            .iter()
-            .fold((0, 0), |(total, max_total), value_opt| match value_opt {
-                None => (0, if total > max_total { total } else { max_total }),
-                Some(value) => (total + value, max_total),
-            });
+    // Calculate totals for each group
+    let mut totals = Vec::new();
+    values.iter().fold(0, |total, value_opt| match value_opt {
+        None => {
+            totals.push(total);
+            0
+        }
+        Some(value) => total + value,
+    });
+    totals.sort();
 
+    // Part 1
+    let max_total: &u32 = totals.iter().last().unwrap();
     println!("Part 1: that elf is carrying {} calories", max_total);
+
+    // Part 2
+    let sum: u32 = totals.iter().rev().take(3).sum();
+    println!("Part 2: those elves are carrying {} calories", sum);
 }
